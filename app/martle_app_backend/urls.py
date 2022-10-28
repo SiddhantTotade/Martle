258ad4595d8f70c021a0b527592e2197b22825cb
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-10-29 00:26:31 +0530

    authentication for user is done

--- app/martle_app_backend/urls.py
+++ app/martle_app_backend/urls.py
@@ -3,6 +3,8 @@ from .views import *
 from .auth_views import *
 
 urlpatterns = [
+
+    # User Authentication
     re_path(r"^register$",RegisterView.as_view()),
     re_path(r"^verify-otp$",VerifyOTP.as_view()),
 ]
