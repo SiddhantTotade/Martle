d6d2261389d88db78e04a19e5e5bc2e17921e38f
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-06-16 00:26:54 +0530

    modified : RandomPickCards.jsx

--- app/martle_app_frontend/src/components/home_page_components/RandomPickCards.jsx
+++ app/martle_app_frontend/src/components/home_page_components/RandomPickCards.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Paper, Typography } from "@mui/material";
 import React from "react";
 
 export default function RandomCardsPick() {
@@ -16,34 +16,13 @@ export default function RandomCardsPick() {
       >
         Tools you might Need
       </Typography>
-      <Box sx={{ display: "flex", gap: "10px" }}>
-        <Box
-          sx={{
-            width: "100%",
-            height:'10vh',
-            border:'2px solid red'
-          }}
-        >
-          <img
-            className="flex justify-center rounded-md"
-            src="https://cdn.shopify.com/s/files/1/1459/5894/files/Black_20Orange_large.gif?v=1685102407"
-            alt="img"
-            width="20%"
-            height="10vh"
-          />
-        </Box>
-        {/* <Box
-          sx={{
-            width: "100%",
-          }}
-        >
-          <img
-            className="flex justify-center rounded-md"
-            src="https://i.gifer.com/9uXq.gif"
-            alt="img"
-            width="20%"
-          />
-        </Box> */}
+      <Box sx={{ width: "90%", display: "flex", gap: "10px" }}>
+        <img
+          src="https://cdn.shopify.com/s/files/1/1459/5894/files/Black_20Orange_large.gif?v=1685102407"
+          alt="img"
+          width="18%"
+          height="8vh"
+        />
       </Box>
     </div>
   );
