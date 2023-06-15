8c732a5f55a87457894ad1769ded1eda6fbc9b2e
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-06-16 02:39:06 +0530

    modified : RandomPickCards.jsx

--- app/martle_app_frontend/src/components/home_page_components/RandomPickCards.jsx
+++ app/martle_app_frontend/src/components/home_page_components/RandomPickCards.jsx
@@ -17,12 +17,41 @@ export default function RandomCardsPick() {
         Tools you might Need
       </Typography>
       <Box sx={{ width: "90%", display: "flex", gap: "10px" }}>
-        <img
-          src="https://cdn.shopify.com/s/files/1/1459/5894/files/Black_20Orange_large.gif?v=1685102407"
-          alt="img"
-          width="18%"
-          height="8vh"
-        />
+        <Box sx={{ position: "relative", width: "100%", borderRadius: "5px" }}>
+          <img
+            className="block rounded-md"
+            src="https://cdn.shopify.com/s/files/1/1459/5894/files/Black_20Orange_large.gif?v=1685102407"
+            alt="img"
+            width="18%"
+          />
+          <Box
+            sx={{
+              width: 280,
+              height: "280px",
+              top: 0,
+              left: 0,
+              position: "absolute",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              opacity: 0,
+              background: "rgba(0,0,0,0.6)",
+              transition: "0.6s",
+              ":hover": { opacity: 1 },
+            }}
+          >
+            <Typography
+              fontWeight={20}
+              fontStyle="oblique"
+              fontFamily="serif"
+              fontSize={30}
+              sx={{ color: "#fff" }}
+            >
+              Ride Safe
+            </Typography>
+          </Box>
+        </Box>
       </Box>
     </div>
   );
