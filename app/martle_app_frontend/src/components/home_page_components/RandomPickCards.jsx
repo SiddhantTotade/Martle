5c539e56fed3a33cb20b7922a55ed913c88087c1
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-06-20 15:52:26 +0530

    modified : github cache

---
+++ app/martle_app_frontend/src/components/home_page_components/RandomPickCards.jsx
@@ -0,0 +1,66 @@
+import { Box, Container, Paper, Typography } from "@mui/material";
+import React from "react";
+
+export default function RandomCardsPick() {
+  return (
+    <div className="w-RandomCards m-auto mt-12">
+      <Typography
+        fontSize={30}
+        sx={{
+          color: "#fff",
+          marginLeft: "10px",
+          marginBottom: "5px",
+          letterSpacing: "2px",
+          fontFamily: "fantasy",
+        }}
+      >
+        Tools you might Need
+      </Typography>
+      <Box sx={{ width: "100%", display: "flex", gap: "10px" }}>
+        <Box
+          sx={{
+            position: "relative",
+            width: "90%",
+            borderRadius: "5px",
+            "@media screen and (max-width: 500px)": { width: "30%" },
+          }}
+        >
+          <img
+            className="block rounded-md sm:w-RandomCards"
+            src="https://cdn.shopify.com/s/files/1/1459/5894/files/Black_20Orange_large.gif?v=1685102407"
+            alt="img"
+            width="18%"
+          />
+          <Box
+            sx={{
+              width: 280,
+              height: "280px",
+              top: 0,
+              left: 0,
+              position: "absolute",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              opacity: 0,
+              background: "rgba(0,0,0,0.6)",
+              transition: "0.6s",
+              ":hover": { opacity: 1 },
+              "@media (max-width: 1368px)": { width: 0 },
+            }}
+          >
+            <Typography
+              fontWeight={20}
+              fontStyle="oblique"
+              fontFamily="serif"
+              fontSize={30}
+              sx={{ color: "#fff" }}
+            >
+              Ride Safe
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
+}
