75f453cc82eb26d2dce5c671cfa1caf41584bd7f
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-07-27 16:13:16 +0530

    modified : ProductImageMagnifier.jsx, ReactSlick.jsx, ProductImageMagnifier.jsx and Product.jsx

--- app/martle_app_frontend/src/components/product_page_components/ProductImageMagnifier.jsx
+++ app/martle_app_frontend/src/components/product_page_components/ProductImageMagnifier.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactImageMagnify from "react-image-magnify";
-import ReactSlick from "react-slick";
+import ReactSlickContainer from "./ReactSlick";
 import { styled } from "@mui/material/styles";
 import { Box } from "@mui/material";
 import watch300 from "../product_page_components/asus_vivobook_2k3hnhx.png";
 import watch1200 from "../product_page_components/wristwatch.jpg";
+import "./styles/productImageMagnifier.css";
 
 const ProductImageMagnifier = () => {
   return (
-    <div class="flex-center">
-      <div class="card flex-center">
-        <section class="product-img">
-          <img src="./Mobile.jpg" alt="mobile" />
-          <div class="magnifier-lens"></div>
-        </section>
-        <section class="product-details">
-          <div class="magnified-img"></div>
-        </section>
+    <div id="magnifier" className="fluid react-slick">
+      <ReactImageMagnify
+        enlargedImagePortalId="portal"
+        {...{
+          smallImage: {
+            alt: "image",
+            width: 400,
+            height: 400,
+            // isFluidWidth: true,
+            src: watch300,
+          },
+          largeImage: {
+            src: watch300,
+            width: 900,
+            height: 900,
+          },
+        }}
+      />
+      <div id="portal" className=""></div>
+      {/* <div className="fluid__image-container">
+        <ReactSlickContainer
+          {...{
+            rimProps: {
+              enlargedImagePortalId: "portal",
+              enlargedImageContainerDimensions: {
+                width: "200%",
+                height: "100%",
+              },
+            },
+          }}
+        />
       </div>
+      <div className="fluid__instructions" style={{ position: "relative" }}>
+        <div id="portal" className="portal"></div>
+      </div> */}
     </div>
   );
 };
--- app/martle_app_frontend/src/pages/Product.jsx
+++ app/martle_app_frontend/src/pages/Product.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../components/base_components/NavBar";
 import Footer from "../components/base_components/Footer";
+import ProductImageMagnifier from "../components/product_page_components/ProductImageMagnifier";
 
 const Product = () => {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
+      <ProductImageMagnifier />
       <Footer />
     </>
   );
