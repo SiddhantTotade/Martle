fe0012400951e10fbe05076231e8a6541d1128ca
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-07-29 00:59:41 +0530

    modified : deleted images

--- app/martle_app_frontend/src/components/product_page_components/ProductImageMagnifier.jsx
+++ app/martle_app_frontend/src/components/product_page_components/ProductImageMagnifier.jsx
@@ -1,49 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ReactImageMagnify from "react-image-magnify";
-import ReactSlickContainer from "./ReactSlick";
 import { styled } from "@mui/material/styles";
 import { Box } from "@mui/material";
-import watch300 from "../product_page_components/asus_vivobook_2k3hnhx.png";
-import watch1200 from "../product_page_components/wristwatch.jpg";
+import i1 from "../product_page_components/wristwatch.jpg";
+import i2 from "../product_page_components/asus.png";
 import "./styles/productImageMagnifier.css";
 
+const images = [i1, i2];
+
 const ProductImageMagnifier = () => {
+  const [img, setImg] = useState(images[0]);
+
+  const handleHover = (image, i) => {
+    setImg(image);
+    // imgRef.current[i].className += " active";
+
+    for (var j = 0; j < images.length; j++) {
+      if (i !== j) {
+        // imgRef.current[j].classList.remove("active");
+      }
+    }
+  };
+
+  const imgRef = useRef([]);
+  imgRef.current = [];
+
+  const addRef = (el) => {
+    if (el && !imgRef.current.includes(el)) {
+      imgRef.current.push(el);
+    }
+  };
   return (
-    <div id="magnifier" className="fluid react-slick">
-      <ReactImageMagnify
-        enlargedImagePortalId="portal"
-        {...{
-          smallImage: {
-            alt: "image",
-            width: 400,
-            height: 400,
-            // isFluidWidth: true,
-            src: watch300,
-          },
-          largeImage: {
-            src: watch300,
-            width: 900,
-            height: 900,
-          },
-        }}
-      />
-      <div id="portal" className=""></div>
-      {/* <div className="fluid__image-container">
-        <ReactSlickContainer
-          {...{
-            rimProps: {
-              enlargedImagePortalId: "portal",
-              enlargedImageContainerDimensions: {
-                width: "200%",
-                height: "100%",
+    <div className="container">
+      <div className="left">
+        <div className="left_1">
+          {images.map((image, i) => {
+            return (
+              <div
+                ref={addRef}
+                onMouseOver={() => handleHover(image, i)}
+                className="img_wrap hover:border-2 hover:border-orange-600"
+                key={i}
+              >
+                <img src={image} alt="img" />
+              </div>
+            );
+          })}
+        </div>
+        <div className="left_2">
+          <ReactImageMagnify
+            {...{
+              smallImage: {
+                alt: "img",
+                isFluidWidth: true,
+                src: img,
               },
-            },
-          }}
-        />
+              largeImage: {
+                alt: "img",
+                src: img,
+                width: 850,
+                height: 850,
+              },
+              enlargedImageContainerDimensions: {
+                width: "150%",
+                height: "150%",
+              },
+            }}
+          />
+        </div>
       </div>
-      <div className="fluid__instructions" style={{ position: "relative" }}>
-        <div id="portal" className="portal"></div>
-      </div> */}
     </div>
   );
 };
