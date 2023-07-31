fd433f7a14a0dad3b0c3e2ed3ea453bc3c553c37
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-08-01 00:34:07 +0530

    modified : ProductAddPurchase.jsx

--- app/martle_app_frontend/src/components/product_page_components/ProductAddPurchase.jsx
+++ app/martle_app_frontend/src/components/product_page_components/ProductAddPurchase.jsx
@@ -1,6 +1,7 @@
-import { Box, Button, Container, IconButton } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React from "react";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ShopIcon from "@mui/icons-material/Shop";
 
 const ProductAddPurchase = () => {
   return (
@@ -12,28 +13,30 @@ const ProductAddPurchase = () => {
         marginTop: "15px",
       }}
     >
-      <IconButton>
-        {" "}
-        <FavoriteBorderIcon />
-      </IconButton>
       <Button
         sx={{
+          height: "7vh",
           width: "100%",
           background: "#fc3838",
           color: "white",
+          gap: "10px",
           ":hover": { background: "#d43131" },
         }}
       >
+        <ShoppingCartIcon />
         Add to cart
       </Button>
       <Button
         sx={{
+          height: "7vh",
           width: "100%",
           background: "#348ee3",
           color: "white",
+          gap: "10px",
           ":hover": { background: "#3083d1" },
         }}
       >
+        <ShopIcon />
         Buy now
       </Button>
     </Box>
