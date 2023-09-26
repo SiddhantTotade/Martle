677829adc3e371e7082dbeb0dcda286b5857a444
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-09-26 19:57:56 +0530

    created : TechnicalDetails section

--- app/martle_app_frontend/src/components/product_page_components/TechnicalDetails.jsx
+++ app/martle_app_frontend/src/components/product_page_components/TechnicalDetails.jsx
@@ -5,34 +5,46 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Container, Typography } from "@mui/material";
 
 const TechnicalDetails = (props) => {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650, height: "1vh" }} aria-label="simple table">
-        <TableBody>
-          {props.product_description
-            ? Object.entries(JSON.parse(props.product_description)).map(
-                ([key, val]) => (
-                  <TableRow key={key}>
-                    <TableCell
-                      style={{ height: "10px" }}
-                      align="left"
-                      component="th"
-                      scope="row"
-                    >
-                      {key}
-                    </TableCell>
-                    <TableCell align="left" component="th" scope="row">
-                      {val}
-                    </TableCell>
-                  </TableRow>
+    <Container sx={{ marginTop: "20px" }}>
+      <Typography fontSize={15} fontWeight={"bold"}>
+        Technical Details
+      </Typography>
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableBody>
+            {props.product_description
+              ? Object.entries(JSON.parse(props.product_description)).map(
+                  ([key, val]) => (
+                    <TableRow key={key}>
+                      <TableCell
+                        sx={{ width: "20%", padding: "5px" }}
+                        style={{ height: "10px" }}
+                        align="left"
+                        component="th"
+                        scope="row"
+                      >
+                        {key}
+                      </TableCell>
+                      <TableCell
+                        sx={{ width: "100%", padding: "5px" }}
+                        align="left"
+                        component="th"
+                        scope="row"
+                      >
+                        {val}
+                      </TableCell>
+                    </TableRow>
+                  )
                 )
-              )
-            : ""}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              : ""}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
