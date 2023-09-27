8ea52f475852695425220b7de46714f4992dafc7
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-09-27 11:32:27 +0530

    created : table for technical details

--- app/martle_app_frontend/src/components/product_page_components/TechnicalDetails.jsx
+++ app/martle_app_frontend/src/components/product_page_components/TechnicalDetails.jsx
@@ -1,46 +1,122 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { Container, Typography } from "@mui/material";
+import { Collapse, Container, Typography } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 
 const TechnicalDetails = (props) => {
+  const technicalDetails = props.product_description
+    ? JSON.parse(props.product_description)
+    : {};
+
+  const { technicalDetailsPreShow, technicalDetailsPostShow } = Object.entries(
+    technicalDetails
+  ).reduce(
+    (acc, [key, value], index) => {
+      const target =
+        index < 5 ? "technicalDetailsPreShow" : "technicalDetailsPostShow";
+      acc[target][key] = value;
+      return acc;
+    },
+    { technicalDetailsPreShow: {}, technicalDetailsPostShow: {} }
+  );
+
+  const dataLength = Object.keys(technicalDetails).length;
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const [showMore, setShowMore] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+    setShowMore(!showMore);
+  };
+
   return (
-    <Container sx={{ marginTop: "20px" }}>
+    <Container sx={{ marginTop: "20px", gap: "5px", display: "grid" }}>
       <Typography fontSize={15} fontWeight={"bold"}>
         Technical Details
       </Typography>
       <TableContainer>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table aria-label="simple table">
           <TableBody>
-            {props.product_description
-              ? Object.entries(JSON.parse(props.product_description)).map(
-                  ([key, val]) => (
-                    <TableRow key={key}>
-                      <TableCell
-                        sx={{ width: "20%", padding: "5px" }}
-                        style={{ height: "10px" }}
-                        align="left"
-                        component="th"
-                        scope="row"
-                      >
-                        {key}
-                      </TableCell>
-                      <TableCell
-                        sx={{ width: "100%", padding: "5px" }}
-                        align="left"
-                        component="th"
-                        scope="row"
-                      >
-                        {val}
-                      </TableCell>
-                    </TableRow>
-                  )
-                )
-              : ""}
+            {Object.entries(technicalDetailsPreShow).map(([key, val]) => (
+              <TableRow>
+                <TableCell
+                  sx={{ width: "20%", padding: "5px" }}
+                  style={{ height: "10px" }}
+                  align="left"
+                  component="th"
+                  scope="row"
+                >
+                  {key}
+                </TableCell>
+                <TableCell
+                  sx={{ width: "20%", padding: "5px" }}
+                  style={{ height: "10px" }}
+                  align="left"
+                  component="th"
+                  scope="row"
+                >
+                  {val}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          {dataLength > 5 ? (
+            showMore ? (
+              ""
+            ) : (
+              <div
+                className="text-sm cursor-pointer text-blue-700 mt-2"
+                onClick={() => handleExpandClick()}
+              >
+                Show more
+                <ExpandMoreIcon />
+              </div>
+            )
+          ) : (
+            ""
+          )}
+          <TableBody>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              {Object.entries(technicalDetailsPostShow).map(([key, val]) => (
+                <TableRow key={key}>
+                  <TableCell
+                    sx={{ width: "20%", padding: "5px" }}
+                    style={{ height: "10px" }}
+                    align="left"
+                    component="th"
+                    scope="row"
+                  >
+                    {key}
+                  </TableCell>
+                  <TableCell
+                    sx={{ width: "20%", padding: "5px" }}
+                    align="left"
+                    component="th"
+                    scope="row"
+                  >
+                    {val}
+                  </TableCell>
+                </TableRow>
+              ))}
+              {showMore ? (
+                <div
+                  className="text-sm cursor-pointer text-blue-700 mt-2"
+                  onClick={() => handleExpandClick()}
+                >
+                  Show less
+                  <ExpandLessIcon />
+                </div>
+              ) : (
+                ""
+              )}
+            </Collapse>
           </TableBody>
         </Table>
       </TableContainer>
